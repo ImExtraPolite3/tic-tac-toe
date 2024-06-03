8b9942ab8f3d5ffa7dde34559591eaaccb6a6caa
ExtraPolite <imextrapolite2@gmail.com>
2024-06-03 16:15:18 -0400

    make factory function in order to create new players

--- game.js
+++ game.js
@@ -1,7 +1,22 @@
 const createGameboard = (function () {
   const Gameboard = {
-    gameboard:['','','','','','','','','']
+    gameboard:[,,,,,,,,]
   };
 
-  return Gameboard;
+  return Gameboard.gameboard;
 })();
+
+function createPlayer (name) {
+  let points = 0;
+  const increasePoints = () => points++
+  const getPoint = () => points;
+
+  return { name, increasePoints, getPoint }
+}
+
+const john = createPlayer('john');
+
+john.increasePoints();
+john.increasePoints();
+
+console.log(john.getPoint());
