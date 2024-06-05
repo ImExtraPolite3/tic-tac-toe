eda80cf929badfe8ecd2f54945756112ee457807
ExtraPolite <imextrapolite2@gmail.com>
2024-06-05 18:09:16 -0400

    fix connectBoard funtion

--- game.js
+++ game.js
@@ -17,25 +17,26 @@ function createPlayer (name, choice) {
 const connectBoard = function () {
   const board = document.querySelectorAll('.board-square');
 
-  board[0].textContent = createGameboard[0];
-  board[1].textContent = createGameboard[1];
-  board[2].textContent = createGameboard[2];
-  board[3].textContent = createGameboard[3];
-  board[4].textContent = createGameboard[4];
-  board[5].textContent = createGameboard[5];
-  board[6].textContent = createGameboard[6];
-  board[7].textContent = createGameboard[7];
-  board[8].textContent = createGameboard[9];
+  createGameboard[0] = board[0].textContent;
+  createGameboard[1] = board[1].textContent;
+  createGameboard[2] = board[2].textContent;
+  createGameboard[3] = board[3].textContent;
+  createGameboard[4] = board[4].textContent;
+  createGameboard[5] = board[5].textContent;
+  createGameboard[6] = board[6].textContent;
+  createGameboard[7] = board[7].textContent;
+  createGameboard[8] = board[8].textContent;
 
-  return board;
+  return createGameboard;
 };
 
 function playGame() {
-  const board = document.querySelectorAll('.board-square');
-  const getNameOne = prompt('player one enter name: ');
-  const getNameTwo = prompt('player two enter name: ');
-  const playerOne = createPlayer(getNameOne, 'X');
-  const playerTwo = createPlayer(getNameTwo, 'O');
+  const board = connectBoard();
+
+  const playerOne = createPlayer('player one', 'X');
+  const playerTwo = createPlayer('player two', 'O');
+
 }
 
+connectBoard;
 playGame();
