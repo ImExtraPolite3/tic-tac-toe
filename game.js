33fcc617b23ab5966cee7cb11ce6a3bd7dc337b7
ExtraPolite <imextrapolite2@gmail.com>
2024-06-05 16:42:01 -0400

    connect board with array

--- game.js
+++ game.js
@@ -14,22 +14,28 @@ function createPlayer (name, choice) {
   return { name, choice, increasePoints, getPoint }
 }
 
+const connectBoard = function () {
+  const board = document.querySelectorAll('.board-square');
+
+  board[0].textContent = createGameboard[0];
+  board[1].textContent = createGameboard[1];
+  board[2].textContent = createGameboard[2];
+  board[3].textContent = createGameboard[3];
+  board[4].textContent = createGameboard[4];
+  board[5].textContent = createGameboard[5];
+  board[6].textContent = createGameboard[6];
+  board[7].textContent = createGameboard[7];
+  board[8].textContent = createGameboard[9];
+
+  return board;
+};
+
 function playGame() {
   const board = document.querySelectorAll('.board-square');
   const getNameOne = prompt('player one enter name: ');
   const getNameTwo = prompt('player two enter name: ');
   const playerOne = createPlayer(getNameOne, 'X');
   const playerTwo = createPlayer(getNameTwo, 'O');
-
-  for (let i = 0; i < 9; i++) {
-    board[i].addEventListener('click', () => {
-      if (i % 2 === 0) {
-        board[i].textContent = playerOne.choice;
-      } else {
-        board[i].textContent = playerTwo.choice;
-      }
-    })
-  }
 }
 
 playGame();
