d1b5453939ac3967fac14c46b7c3e2a157c993b1
ExtraPolite <imextrapolite2@gmail.com>
2024-06-06 16:41:56 -0400

    use loop for connectBoard

--- game.js
+++ game.js
@@ -16,15 +16,9 @@ function createPlayer (name, choice) {
 
 const connectBoard = function () {
   const board = document.querySelectorAll('.board-square');
-  createGameboard[0] = board[0].textContent;
-  createGameboard[1] = board[1].textContent;
-  createGameboard[2] = board[2].textContent;
-  createGameboard[3] = board[3].textContent;
-  createGameboard[4] = board[4].textContent;
-  createGameboard[5] = board[5].textContent;
-  createGameboard[6] = board[6].textContent;
-  createGameboard[7] = board[7].textContent;
-  createGameboard[8] = board[8].textContent;
+  for (let i = 0; i < 8; i++) {
+    createGameboard[i] = board[i].textContent;
+  }
 
   return createGameboard;
 };
