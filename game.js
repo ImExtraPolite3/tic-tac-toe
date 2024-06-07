0c27ad867f6df9e0aab301503fef627ba02f61bd
ExtraPolite <imextrapolite2@gmail.com>
2024-06-06 20:37:15 -0400

    complete winCondition

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ function createPlayer (name, choice) {
 
 const connectBoard = function () {
   const board = document.querySelectorAll('.board-square');
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 9; i++) {
     createGameboard[i] = board[i].textContent;
   }
 
@@ -31,17 +31,18 @@ function userInput() {
 
   board.forEach(eachSquare => {
     eachSquare.addEventListener('click', () => {
-      if (num < 9 && num % 2 === 0) {
+      if (num < 9 && num % 2 === 0 && winCondition() !== 'player one wins') {
         eachSquare.textContent = playerOne.choice;
         // console.log(winCondition());
         num++;
-      } else if (num < 9 && num % 2 !== 0) {
+      } else if (num < 9 && num % 2 !== 0 && winCondition() !== 'player two wins') {
         eachSquare.textContent = playerTwo.choice;
         // console.log(winCondition());
         num++;
       } 
       // console.log(connectBoard());
-      winCondition();
+      // winCondition();
+      console.log(winCondition());
     })
   })
 }
@@ -50,13 +51,18 @@ const winCondition = function () {
   const squares = connectBoard();
   const combination = {
     line1: [0, 1, 2],
-    line2: [0, 4, 8]
+    line2: [3, 4, 5],
+    line3: [6, 7, 8],
+    line4: [0, 4, 8],
+    line5: [2, 4, 6]
   };
   const something = Object.values(combination);
 
   for (let i = 0; i < something.length; i++) {
     if (squares[something[i][0]] === 'X' && squares[something[i][1]] === 'X' && squares[something[i][2]] === 'X') {
-      console.log('player one wins');
+      return 'player one wins';
+    } else if (squares[something[i][0]] === 'O' && squares[something[i][1]] === 'O' && squares[something[i][2]] === 'O') {
+      return 'player two wins';
     }
   }
 }
